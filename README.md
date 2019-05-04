d0e3c656bebb0a7597ad1e4cc53c5bc0a97b8059
Aerevia <rayanslimani@gmail.com>
2019-05-04 13:04:13 +0200

    docker

---
+++ README.md
@@ -0,0 +1,11 @@
+# screeps-docker
+Setup a standalone screeps server with mongo + redis
+
+# Instructions
+Create a ".env" file with your [Steam API Key](https://steamcommunity.com/dev/apikey) defined like this:
+
+    STEAMKEY="XXX"
+
+Start the server with 
+
+    sudo docker-compose up 
---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# config check
+cd /screeps
+
+if [ ! -e /screeps/.screepsrc ]; then
+    if [ -z "$STEAMKEY" ]; then
+        echo "Did you forget to set the STEAMKEY environment variable?"
+        exit 1
+    else
+        # pass our steam key into the init process
+        echo "initializing screeps..."
+        echo "${STEAMKEY}" | screeps init
+
+        echo "initializing mods..."
+	# should move back into the Dockerfile
+        yarn add screepsmod-mongo screepsmod-auth screepsmod-tickrate screepsmod-admin-utils screepsmod-features @screeps/simplebot
+
+        echo "initializing .screepsrc..."
+        echo -e "\n[mongo]" >> .screepsrc
+        echo "host=${MONGO_LINK}" >> .screepsrc
+        echo "[redis]" >> .screepsrc
+        echo "host=${REDIS_LINK}" >> .screepsrc
+        mv custom_mods.json mods.json
+    fi
+fi
+
+screeps start
